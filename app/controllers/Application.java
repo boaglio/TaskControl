65ecdb48c62cb674dd121bd2c0b79c51505cfe7c
Eduardo Cerqueira <eduardo.cerqueira@eduardocerqueir.(none)>
2012-05-08 00:09:29 -0300

    Area de admin

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -28,52 +28,6 @@ public class Application extends Controller {
     	render(task, users, taskTypes);
     }
     
-    @Check("administrator")
-    public static void formUser(Long id) {
-    	List<User> users = User.findAll();
-    	
-    	if(id == null) {
-    		render(users);
-    	}
-    	
-    	User user = User.findById(id);
-    	render(users,user);
-    }
-    
-    @Check("administrator")
-    public static void formTaskType(Long id) {
-    	List<TaskType> taskTypes = TaskType.findAll();
-    	
-    	if (id == null) {
-    		render(taskTypes);
-		}
-    	
-    	TaskType taskType = TaskType.findById(id);
-    	render(taskTypes, taskType);
-    }
-    
-    public static void saveTasktype(TaskType tasktype) {
-    	tasktype.save();
-    	index();
-    }
-    
-    public static void deleteTaskType(Long id) {
-    	TaskType taskType = TaskType.findById(id);
-    	taskType.delete();
-    	formTaskType(0L);
-    }
-    
-    public static void saveUser(User user) {
-    	user.save();
-    	index();
-    }
-    
-    public static void deleteUser(Long id ) {
-    	User user = User.findById(id);
-    	user.delete();
-    	formUser(0L);
-    }
-    
     public static void saveTask(Task task) {
     	task.save();
     	index();
